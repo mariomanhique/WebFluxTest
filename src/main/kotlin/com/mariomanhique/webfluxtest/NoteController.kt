08f0257748bf741597fd3d0e5f10ef2331feda40
Mario Manhique <79359430+MErnesto99@users.noreply.github.com>
2025-06-03 22:32:26 +0100

    logging

--- src/main/kotlin/com/mariomanhique/webfluxtest/NoteController.kt
+++ src/main/kotlin/com/mariomanhique/webfluxtest/NoteController.kt
@@ -3,10 +3,8 @@ package com.mariomanhique.webfluxtest
 import lombok.RequiredArgsConstructor
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -18,10 +16,11 @@ import reactor.core.publisher.Mono
 @RequestMapping("api/v1/notes")
 class NoteController @Autowired constructor(private val noteService: NoteService) {
 
-//    private val logger = LoggerFactory.getLogger(NoteController::class.java)
+    private val logger = LoggerFactory.getLogger(NoteController::class.java)
 
     @PostMapping("/save")
     fun saveNote(@RequestBody note: Note): Mono<Note> {
+        logger.info("Saving note $note")
         return noteService.save(note)
     }
 
